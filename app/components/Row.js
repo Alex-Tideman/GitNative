71dfccde67c770b7c8d2afd664f89a0e31d763f6
Alex Tideman <alex.tideman@gmail.com>
2016-12-08 14:09:30 -0700

    Update row styles

--- app/components/Row.js
+++ app/components/Row.js
@@ -18,6 +18,23 @@ export default class Row extends Component{
 
   render() {
     const { book } = this.props
+    let score = book.volumeInfo.averageRating ? book.volumeInfo.averageRating * 12 : 2
+    if(score >= 50) {
+      scoreColor = '#1E77E2'
+    }
+    if(score > 40 && score < 50) {
+      scoreColor = '#6A5'
+    }
+    if(score > 30 && score < 40) {
+      scoreColor = '#FED024'
+    }
+    if(score > 20 && score < 30) {
+      scoreColor = '#FA5732'
+    }
+    if(score < 20) {
+      scoreColor = '#C21515'
+    }
+
     if(book.size < 1) {
       return (
         <View>
@@ -29,12 +46,17 @@ export default class Row extends Component{
     if(book.volumeInfo.authors && book.volumeInfo.authors.length > 0) {
       authors = book.volumeInfo.authors.join(', ')
     }
+    if(book.volumeInfo.averageRating) {
+      rating = book.volumeInfo.averageRating
+    } else {
+      rating = "N/A"
+    }
     return (
       <View style={styles.bookRow}>
-        <Text style={[styles.bookInfo, styles.bookTitle]}>Title: {book.volumeInfo.title}</Text>
+        <Text style={[styles.bookInfo, styles.bookTitle]}>{book.volumeInfo.title}</Text>
         <Text style={[styles.bookInfo, styles.bookAuthor]}>Author: {authors}</Text>
-        <Text style={[styles.bookInfo, styles.bookRating]}>Average Rating: {book.volumeInfo.averageRating}</Text>
-        <Text style={[styles.bookInfo, styles.bookPageCount]}># of Pages: {book.volumeInfo.pageCount}</Text>
+        <Text style={[styles.bookInfo, styles.bookRating]}>Average rating of {rating}</Text>
+        <Text style={[styles.bookInfo, styles.bookPageCount]}>{book.volumeInfo.pageCount} pages</Text>
       </View>
     );
   }
@@ -46,11 +68,25 @@ const styles = StyleSheet.create({
     padding: 10,
     margin: 3,
     flexDirection: 'column',
-    backgroundColor: '#eee',
+    backgroundColor: '#fff',
   },
-  bookInfo: {
+  bookTitle: {
     fontSize: 20,
+    fontWeight: '500',
   },
+  bookAuthor: {
+    fontSize: 16,
+    fontWeight: '100',
+  },
+  bookRating: {
+    fontSize: 16,
+    fontWeight: '100',
+  },
+  bookPageCount: {
+    fontSize: 16,
+    fontWeight: '100',
+  },
+
   bookAvatar: {
     flex: 3,
     height: 30,
