13e68050da025e232db7a105f390dc09115aa9a4
Alex Tideman <alex.tideman@gmail.com>
2016-12-13 13:35:43 -0700

    Add immutable to package json

--- app/containers/booksContainer.js
+++ app/containers/booksContainer.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux'
 import { actionCreators } from '../actions/bookActions'
+import Immutable from 'immutable'
 
 const mapStateToProps = (state) => {
   return { books: state.books }
